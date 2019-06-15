88f59f94d8cc44406340b723a4af46eb7b104a5a
Dongho Oh <graystone117@gmail.com>
2019-06-15 22:07:23 +0900

    delete JavaFX

--- src/main/java/org/odhs/happydinner/main/HappyDinner.java
+++ src/main/java/org/odhs/happydinner/main/HappyDinner.java
@@ -1,19 +1,8 @@
 package org.odhs.happydinner.main;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
-
-public class HappyDinner extends Application {
-
-    @Override
-    public void start(Stage stage) throws Exception {
-        stage.show();
-
-        stop();
-    }
+public class HappyDinner {
 
     public static void main(String[] args) {
         System.out.println("00");
-        launch(args);
     }
 }
